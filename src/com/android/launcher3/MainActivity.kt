7280da1144a6548940427aebe85762b069d38955
Sviatoslav Koliesnik <kolesniksy@gmail.com>
2018-03-20 11:20:46 +0200

    comment MainActivity in manifest

--- src/com/android/launcher3/MainActivity.kt
+++ src/com/android/launcher3/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.android.launcher3
 
+import android.annotation.SuppressLint
 import android.app.Activity
 
 /**
@@ -7,5 +8,5 @@ import android.app.Activity
  * @since 2/21/18
  * Temporary activity. Remove later.
  */
-//TODO: remove when finish
+@SuppressLint("Registered")
 class MainActivity : Activity()
